0ef918117bd3dc279bf4872138c86d194338c817
Thomas Dunlap <ttdunlap33@gmail.com>
2021-11-01 13:31:07 -0600

    removing unused files and code

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,29 +1,7 @@
-// const mysql      = require('mysql');
-// const connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'root',
-//   password : 'root',
-//   database : 'instantmesseging'
-// });
-
-// connection.connect(function(err) {
-//     if (err) {
-//       console.error('error connecting: ' + err.stack);
-//       return;
-//     }
-   
-//     console.log('connected as id ' + connection.threadId);
-//   });
-
-// connection.connect(function(err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-//   });
-
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "password",
+    PASSWORD: "root",
     DB: "instantmesseging",
     dialect: "mysql",
     pool: {
